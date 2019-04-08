f812873ccdd31b25976a488f83050b25a90e0ae1
yenchen-liu <r96725046@g.ntu.edu.tw>
2019-04-08 14:29:18 +0800

    add files for model

---
+++ copy_model.py
@@ -0,0 +1,26 @@
+from object_tracer.core.dlf_repository import FUNC2MODEL
+import os
+import shutil
+
+
+parent_path=os.path.dirname(os.path.realpath(__file__))
+dlf_source = os.path.dirname(parent_path)+"/dlf_source/inference/model"
+
+print FUNC2MODEL
+output = parent_path+"/deep_learning_framework/inference/model"
+print dlf_source
+for key in FUNC2MODEL:
+    framework = FUNC2MODEL[key][0]
+    model_name = FUNC2MODEL[key][1]
+    src_path =  dlf_source+"/"+framework+"/"+model_name
+    dest = output+"/"+framework+"/"+model_name
+    #print src_path
+    #print dest
+    if not os.path.exists(dest):
+#        os.makedirs(dest)
+        print "copy dest:"+dest
+        shutil.copytree(src_path,dest)
+
+
+#print parent_path+"/deep_learning_framework/inference/model"
+#print os.path.exists(parent_path+"/deep_learning_framework/inference/model")
