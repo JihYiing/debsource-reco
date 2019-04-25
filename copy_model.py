ee1bb65c2e17857ed9bc1712618f445d21e1bc2e
Yen-Chen Liu <42165511+yenchen-liu@users.noreply.github.com>
2019-04-25 16:36:16 +0800

    Changing dlf dest path

--- copy_model.py
+++ copy_model.py
@@ -4,10 +4,10 @@ import shutil
 
 
 parent_path=os.path.dirname(os.path.realpath(__file__))
-dlf_source = os.path.dirname(parent_path)+"/dlf_source/inference/model"
+dlf_source = os.path.dirname(parent_path)+"/object_tracer/libs/dlf/inference/model"
 
 print FUNC2MODEL
-output = parent_path+"/deep_learning_framework/inference/model"
+output = parent_path+"/model"
 print dlf_source
 for key in FUNC2MODEL:
     framework = FUNC2MODEL[key][0]
